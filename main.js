43957e52ac956165cf6ce2b8f545193d5ce39d18
sennakhl <sennakhl@c3r9p8.1337.ma>
2024-11-17 13:07:50 +0100

    add fucntion to count the totale amount

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ let dateI = document.querySelector(".dateI");
 let amountI = document.querySelector(".amountI");
 
 document.addEventListener("click", function (e) {
-  if (e.target.className === "delet") e.target.parentNode.remove();
+  if (e.target.className === "delet") {
+    e.target.parentNode.remove();
+    printTotal();
+  }
 });
 
 function creatDeletButton(parent) {
@@ -28,6 +31,7 @@ function addTd(nameI, dateI, amountI) {
   tr.appendChild(d);
 
   a.textContent = amountI.value;
+  a.className = "prix";
   tr.appendChild(a);
 
   creatDeletButton(tr);
@@ -46,16 +50,41 @@ let checkEmpty = function (input) {
   return 0;
 };
 
+function printTotal() {
+  let prices = document.querySelectorAll(".prix");
+  let par = document.querySelector(".empty");
+  let span = document.querySelector("span");
+  let total = 0;
+
+  for (let prix of prices) {
+    total += isNaN(prix.textContent) ? 0 : +prix.textContent;
+  }
+  if (prices.length !== 0) {
+    par.textContent = `The totale amounts is:`;
+    par.style.backgroundColor = "transparent";
+    par.style.border = "0";
+    span.textContent = `${total}`;
+    par.style.textAlign = "center";
+    par.style.marginTop = "50px";
+  } else {
+    par.style.backgroundColor = "#cac9c9";
+    par.style.width = "calc(90vw - 10px);";
+    par.style.textAlign = "left";
+    par.style.marginTop = "0";
+    par.textContent = "No expense added yet!";
+    span.textContent = "";
+  }
+}
+
 but.onclick = function () {
   let b = 0;
   b += checkEmpty(nameI);
   b += checkEmpty(dateI);
   b += checkEmpty(amountI);
   if (b !== 0) return;
-  let par = document.querySelector(".empty");
-  if (par) par.remove();
   addTd(nameI, dateI, amountI);
   nameI.value = "";
   dateI.value = "";
   amountI.value = "";
+  printTotal();
 };
